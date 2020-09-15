e3067e81257b0ff4f66b4d84d161c334b3468f88
GavynBevington <35120045+GavynBevington@users.noreply.github.com>
2020-09-15 09:04:53 -0400

    Create OpenCVAutoTesting.java

---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVAutoTesting.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous [name="openCVauto"]
+public class OpenCVAutoTesting extends LinearOpMode
+{
+    @Override
+    public void runOpMode []
+        {
+
+    }
+}
